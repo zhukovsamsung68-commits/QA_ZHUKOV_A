bbda249517dbbc3727f37f4eb642d2ca442ead1c
zhukovsamsung68-commits <zhukov.samsung68@gmail.com>
2025-11-15 17:07:38 +0300

    Delete MyArraySizeException.java

--- MyArraySizeException.java
+++
@@ -1,9 +0,0 @@
-public class MyArraySizeException extends Exception {
-    public MyArraySizeException() {
-        super("Массив должен иметь размер 4x4");
-    }
-
-    public MyArraySizeException(String message) {
-        super(message);
-    }
-}
