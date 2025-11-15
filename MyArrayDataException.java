aa11289363b4d2c87e53690aca7c22dc8ef3f145
zhukovsamsung68-commits <zhukov.samsung68@gmail.com>
2025-11-15 17:07:30 +0300

    Delete MyArrayDataException.java

--- MyArrayDataException.java
+++
@@ -1,9 +0,0 @@
-public class MyArrayDataException extends Exception {
-    public MyArrayDataException() {
-        super("Ошибка данных в массиве");
-    }
-
-    public MyArrayDataException(String message) {
-        super(message);
-    }
-}
